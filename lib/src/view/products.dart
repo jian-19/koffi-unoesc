4fd8140825fdeca516b180ee10ea9d24bdef0354
jian-19 <jian-ferreira@hotmail.com>
2025-07-13 17:48:03 -0300

    refactor display img and names category

--- lib/src/view/products.dart
+++ lib/src/view/products.dart
@@ -91,7 +91,6 @@ class ProductsScreen extends StatelessWidget {
           ),
           const SizedBox(height: 16), 
 
-          // LANCHES
           GestureDetector(
             onTap: () {
               
